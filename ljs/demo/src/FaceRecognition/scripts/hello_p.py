f0a8dd775e9c07236a549812b249ad99fc500d93
shanhe321 <19092413833@163.com>
2024-09-17 10:19:49 +0800

    Add files via upload

---
+++ ljs/demo/src/FaceRecognition/scripts/hello_p.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+import rospy
+
+if __name__ == "__main__":
+
+    rospy.init_node("Hello_Vscode_p")
+    rospy.loginfo("Hello VScode, 我是 Python ....")
+
+    # /home/shanhe/faces_to_read/4.jpg
---
+++ ljs/demo/src/FaceRecognition/src/hello.cpp
@@ -0,0 +1,8 @@
+#include "ros/ros.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout<<"hello";
+    return 0;
+}
