df20b3e04319feae495f1b9ff6476899a4210cf4
wsgcz <2252738795@qq.com>
2024-11-03 14:02:13 +0800

    gcz add commitment

--- lzh/test/src/map_build/src/lidar_distance.cpp
+++ lzh/test/src/map_build/src/lidar_distance.cpp
@@ -3,8 +3,8 @@
 #include <std_msgs/Float32.h>
 #include <sensor_msgs/LaserScan.h>
 
-int frame = 1;
-const int RANGE = 3;
+int frame = 1; //当frame为0时，向/home_service/robot_getdistance，否则只接受lidar的发布信息而不发送。设置初始值为1。
+const int RANGE = 3; //取雷达前方的三个点
 ros::Publisher lidar_pub;
 std_msgs::Float32 msg_distance;
 
@@ -13,24 +13,28 @@ std_msgs::Float32 msg_distance;
  */
 void request_callback(const std_msgs::String::ConstPtr& msg) {
     if (msg->data == "true") {
-        frame = 0;
+        frame = 0; //将frame设置为0，表示可以发送雷达距离
         printf("----------lidar get message-----------\n");
     }
 }
 
+/**
+ * 机器人雷达信息的回调函数，计算前方三个点的平均距离并向/home_service/robot_getdistance发布
+ */
 void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
-    if (frame == 1){
+    if (frame == 1){ //如果frame==1，说明/home_service/lidar_distance向你发送他
         return;
     }
 
-    int nNum = scan->ranges.size();
-    float distance_sum = 0;
+    int nNum = scan->ranges.size();//雷达发送消息总数
+    float distance_sum = 0; 
     float distance_average = 0;
     int count = 0;
-    int number_start = (nNum / 2)  - (RANGE / 2);
+    int number_start = (nNum / 2)  - (RANGE / 2); //获得正前方RANGE个点的平均距离
     for (int i = 0; i < RANGE; i += 1){
         if (!(scan->ranges[number_start + i] < 2 && scan->ranges[number_start + i] > 0)){
+            //如果距离过大或是过小，则不计算该点，用于避免雷达传回的inf值
             continue;
         }
         distance_sum += scan->ranges[number_start + i];
@@ -38,16 +42,18 @@ void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
         printf("-----distance %d is %f", number_start + i, scan->ranges[number_start + i]);
     }
     if (count == 0) {
+        //如果说所有点都过大或者是过小，则说明机器人不是正对垃圾桶，则直接丢垃圾
         msg_distance.data = 55;
         lidar_pub.publish(msg_distance);
     }
     else {
+        //返回符合条件的所有点的平均距离
         distance_average = distance_sum / count;
         ROS_INFO("-----------the average distance is %f------------", distance_average);
         msg_distance.data = distance_average;
         lidar_pub.publish(msg_distance);
     }
-    frame = 1;
+    frame = 1;//frame设置为1，不再发布雷达获得的距离
     
 }
 
--- lzh/test/src/map_build/src/waypoint.cpp
+++ lzh/test/src/map_build/src/waypoint.cpp
@@ -15,13 +15,18 @@ static ros::Publisher vel_pub;
 static ros::Publisher waypoint_add;
 tf2_ros::Buffer buffer;
 
-
+/**
+ * 将速度重置为0并且发布
+ */
 void reset_vel(geometry_msgs::Twist& vel_cmd) {
     vel_cmd.linear.x = 0;
     vel_cmd.angular.z= 0;
     vel_pub.publish(vel_cmd);
 }
 
+/**
+ * 获得机器人当前在地图坐标系的pose
+ */
 geometry_msgs::Pose get_mappose(){
     geometry_msgs::Pose pose;
 
@@ -55,10 +60,10 @@ int main(int argc, char* argv[])
     tf2_ros::TransformListener listener(buffer);
 
     vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
-    waypoint_add = n.advertise<waterplus_map_tools::Waypoint>("waterplus/add_waypoint",10);
-    waterplus_map_tools::Waypoint newWayPoint;
-    newWayPoint.frame_id = "map";
-    int waypoint_count = 0;
+    waypoint_add = n.advertise<waterplus_map_tools::Waypoint>("waterplus/add_waypoint",10);//向waterplus模块发送添加航点的消息
+    waterplus_map_tools::Waypoint newWayPoint; //航点数据类型
+    newWayPoint.frame_id = "map"; //航点的坐标系为地图坐标系
+    int waypoint_count = 0; //航点命名，从0开始依次累加
     ros::Rate rate = ros::Rate(0.4);
     geometry_msgs::Twist vel_cmd;
     vel_cmd.linear.x = 0;
@@ -69,6 +74,17 @@ int main(int argc, char* argv[])
     vel_cmd.angular.y = 0;
     vel_cmd.angular.z = 0;
 
+    /**
+     * 键盘输入1，前进
+     * 键盘输入2，后退
+     * 键盘输入3，左转
+     * 键盘输入4，右转
+     * 键盘输入5，小幅度前进
+     * 键盘输入6，小幅度后退
+     * 键盘输入7，小幅度左转
+     * 键盘输入8，小幅度右转
+     * 键盘输入c，在当前位置以及机器人的朝向在地图上添加航点
+     */
     while (ros::ok())
     {
         char c = getchar();
@@ -110,7 +126,7 @@ int main(int argc, char* argv[])
                 newWayPoint.name = std::to_string(waypoint_count);
                 newWayPoint.pose = get_mappose();
                 waypoint_add.publish(newWayPoint);
-                waypoint_count += 1;
+                waypoint_count += 1; //累加航点的名字
                 break;
             default:
                 break;
